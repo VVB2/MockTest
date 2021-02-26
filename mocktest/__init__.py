b8dd9c773131e6c93bc146f2dcf7b03e822e4483
Vinod Bhat <Vinodvamanbhat@gmail.com>
2021-02-26 11:13:20 +0530

    Done with the password reset email sending feature

--- mocktest/__init__.py
+++ mocktest/__init__.py
@@ -18,8 +18,8 @@ login_manager.login_message_category = 'warning'
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASSWORD')
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
 mail = Mail(app)
 
 from mocktest import routes
