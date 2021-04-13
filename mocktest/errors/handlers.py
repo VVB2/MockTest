fd1490f74b85fe58253d2de2037f5e495fdf4ad1
VVB2 <vinodvamanbhat@gmail.com>
2021-04-13 17:57:45 +0530

    better ui design

--- mocktest/errors/handlers.py
+++ mocktest/errors/handlers.py
@@ -1,18 +1,19 @@
 from flask import Blueprint, render_template
+from flask_login import current_user
 
 errors = Blueprint('errors', __name__)
 
 
 @errors.app_errorhandler(404)
 def error_404(error):
-    return render_template('errors/404.html'), 404
+    return render_template('errors/404.html', image_file=current_user.image_file), 404
 
 
 @errors.app_errorhandler(403)
 def error_403(error):
-    return render_template('errors/403.html'), 403
+    return render_template('errors/403.html', image_file=current_user.image_file), 403
 
 
 @errors.app_errorhandler(500)
 def error_500(error):
-    return render_template('errors/500.html'), 500
+    return render_template('errors/500.html', image_file=current_user.image_file), 500
